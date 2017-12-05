a9b992c2a9bfc0f3d4c2ec57b135163b17e55278
Hilda <hilda.sandberg@gmail.com>
2017-12-05 20:51:22 +0100

    update

--- code/src/components/station.js
+++ code/src/components/station.js
@@ -2,24 +2,30 @@ import React from "react"
 
 class Station extends React.Component {
 
-render() {
-    return (
-      <div className="stationHolder" style={{ backgroundColor: `#${this.props.radioCol}` }}>
-        <div className="logoHolder">
-          <div className="logoImg" style={{ backgroundImage: `url(${this.props.radioImg})` }} />
-        </div>
+handleMetaDataLoad = () => {
+  this.setState({
+    ready: true
+  })
+}
 
-        <div className="playerHolder">
-          <div className="stationInfo">
-            {this.props.radioTag}
-          </div>
-          <audio className="audioHolder" controls>
-            <source src={this.props.radioUrl} type="audio/ogg" />
-          </audio>
-        </div>
+render() {
+  return (
+    <div className="stationHolder" style={{ backgroundColor: `#${this.props.radioCol}` }}>
+      <div className="logoHolder">
+        <div className="logoImg" style={{ backgroundImage: `url(${this.props.radioImg})` }} />
       </div>
-    )
-  }
+
+      <div className="playerHolder">
+        <div className="stationInfo">
+          {this.props.radioTag}
+        </div>
+        <audio className="audioHolder" controls onLoadedMetadata={this.handleMetaDataLoad}>
+          <source src={this.props.radioUrl} type="audio/ogg" />
+        </audio>
+      </div>
+    </div>
+  )
+}
 }
 
 export default Station
